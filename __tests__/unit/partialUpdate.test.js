673a15101918bf80fbf87a90a74a72298ec1bab8
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-12-18 20:40:26 -0600

    Add passing test for partialUpdate.js helper

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,14 @@
+const sqlForPartialUpdate = require("../../helpers/partialUpdate");
+
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
 
+    const { query, values } = sqlForPartialUpdate("jobs", {min_salary: 70000}, "id", 1);
+    // const { query, values } = sqlForPartialUpdate(companies, {num_employees: 203}, handle, "target");
+    expect(query).toEqual(`UPDATE jobs SET min_salary=$1 WHERE id=$2 RETURNING *`);
+    expect(values).toEqual([70000, 1]);
   });
 });
