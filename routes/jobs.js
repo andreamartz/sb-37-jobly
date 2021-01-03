f8b2afe3964bc386f6ddeef95c3b5d0889079ef7
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-02 22:12:29 -0600

    Add the GET route for finding multiple jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,32 +10,32 @@ const jobSchemaNew = require("../schemas/jobSchemaNew");
 const validateData = require("../helpers/validateData");
 
 
+router.get("/", async function (req, res, next) {
+  try {
+    let { search, min_salary, min_equity } = req.query;
+    const data = {};
 
-// router.get("/", async function (req, res, next) {
-//   try {
-//     let {search, min_employees, max_employees} = req.query;
-//     const data = {};
+    if (search !== undefined) {
+      search = search.toLowerCase();
+      data.search = search;
+    }
 
-//     if (search !== undefined) {
-//       search = search.toLowerCase();
-//       data.search = search;
-//     }
+    if (min_salary !== undefined) {
+      data.min_salary = min_salary;
+    }
 
-//     if (min_employees !== undefined) {
-//       data.min_employees = min_employees;
-//     }
+    if (min_equity !== undefined) {
+      data.min_equity = min_equity;
+    }
 
-//     if (max_employees !== undefined) {
-//       data.max_employees = max_employees;
-//     }
+    console.log("DATA: ", data);
+    const results = await Job.findAll(data);
 
-//     const results = await Company.findAll(data);
-
-//     return res.json({companies: results});
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+    return res.json({jobs: results});
+  } catch (err) {
+    return next(err);
+  }
+});
 
 router.post("/", async function(req, res, next) {
   try {
