8654696eec88f873c480e24273f55d67136b398b
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-03 15:57:03 -0600

    Add delete route for jobs and remove method on Job class

--- models/job.js
+++ models/job.js
@@ -117,6 +117,23 @@ class Job {
     console.log("RESULTS.ROWS[0]: ", results.rows[0]);
     return results.rows[0];
   }
+
+  static async remove(id) {
+    id = +id;
+    console.log("ID: ", id);
+    const result = await db.query(
+      `DELETE FROM jobs 
+      WHERE id = $1
+      RETURNING 
+        id`,
+      [ id ]
+    );
+
+    if (result.rows.length === 0) {
+      throw new ExpressError('No such job was found', 404);
+    }
+    return result.rows[0];
+  }
 }
 
   module.exports = Job;
