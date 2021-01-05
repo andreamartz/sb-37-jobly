994955eb68a1d6bae6cd9b3b5641ab1f2853df87
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-04 18:21:16 -0600

    Add update route for users

--- routes/users.js
+++ routes/users.js
@@ -47,4 +47,38 @@ router.post("/", async function(req, res, next) {
   }
 });
 
+router.patch("/:username", async function (req, res, next) {
+  try {
+    const username = req.params.username.toLowerCase();
+    const userData = req.body.user;
+
+    // throw error if username is not found for any user
+    const UserCheck = await User.findOne(username);
+
+    if (userData.username) {
+      userData.username = userData.username.toLowerCase();
+      // throw error if user tries to update the username
+      if (userData.username !== username) {
+        throw new ExpressError('You cannot change the username.', 400);
+      }
+    }
+
+    // validate the data on the request body
+    const validationOutcome = validateData(req.body, userSchemaUpdate);
+
+    // pass any validation errors to error handler
+    if (validationOutcome instanceof Error) {
+      return next(validationOutcome);
+    }
+
+    // at this point, the request data have been confirmed valid
+    // do the update in the database; save to 'user' variable
+    const user = await User.update(username, userData);
+
+    return res.json({ user });
+  } catch (err) {
+      return next(err);
+  }
+});
+
 module.exports = router;
