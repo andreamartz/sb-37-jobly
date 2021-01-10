526fd884916552eccb68616d06da98585570a01d
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-09 22:14:53 -0600

    Add four passing tests of the user registration route

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -5,8 +5,8 @@ const app = require("../../app");
 
 const {
   TEST_DATA,
-  afterEachHook,
   beforeEachHook,
+  afterEachHook,
   afterAllHook
 } = require("./config");
 
@@ -21,6 +21,101 @@ afterEach(async () => {
 afterAllHook(async () => {
   await afterAllHook();
 });
+
+describe("POST /register", function () {
+  test("Registers a new user", async function () {
+    const user = { 
+      user: 
+      { username: "testuser",
+        password: "testpw",
+        first_name: "Test",
+        last_name: "User",
+        email: "test@user.com",
+        photo_url: "https://placekitten.com/250/350",
+        is_admin: "false" 
+      }
+    };
+    const response = await request(app)
+      .post('/auth/register')
+      .send(user)
+    ;
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({ token: expect.any(String) });
+  });
+
+  test("Prevents registering a user when an extra property on the request body", async function() {
+    const user = { 
+      user: 
+      { username: "testuser-admin",
+        password: "testpw",
+        first_name: "Test",
+        last_name: "User",
+        email: "test@user.com",
+        photo_url: "https://placekitten.com/250/350",
+        is_admin: "false",
+        cat: "Goldie Mae"
+      }
+    };
+    const response = await request(app)
+      .post("/auth/register")
+      .send(user)
+    ;
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({
+      "status": 400,
+      "message": [
+        "instance.user is not allowed to have the additional property \"cat\""
+      ]
+    });
+  });
+
+  test("Prevents registration/creation of a user with duplicate username", async function() {
+    const user = { 
+      user: 
+      { username: "testuser-admin",
+        password: "testpw",
+        first_name: "Test",
+        last_name: "User",
+        email: "test@user.com",
+        photo_url: "https://placekitten.com/250/350",
+        is_admin: "false" 
+      }
+    };
+    const response = await request(app)
+      .post("/auth/register")
+      .send(user)
+    ;
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({
+      "status": 400,
+      "message": `The username '${user.user.username}' is taken; please choose another.`
+    });
+  });
+
+  test("Prevents registration/creation of a user if no password is provided", async function () {
+    const user = { 
+      user: 
+        { username: "testuser",
+          first_name: "Test",
+          last_name: "User",
+          email: "test@user.com",
+          photo_url: "https://placekitten.com/250/350",
+          is_admin: "false" 
+        }
+    };
+    const response = await request(app)
+      .post('/auth/register')
+      .send(user);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(
+      { 
+        "message": ["instance.user requires property \"password\""], "status": 400 
+      }
+    );
+  });
+});
+
+
 
 // describe("GET /users", () => {
 //   test("Gets all users", async () => {
