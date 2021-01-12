fea5127647f2aae00ac7059f16dc5195f87b62fa
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-12 11:46:34 -0600

    Add tests of users routes and comments to document which tests are where

--- __tests__/integration/users.test.js
+++ __tests__/integration/users.test.js
@@ -122,9 +122,9 @@ describe("POST /auth/register", function () {
   });
 });
 
-/** 
- * Test /auth/login route
- */
+// /** 
+//  * Test /auth/login route
+//  */
 describe("POST /auth/login", function () {
   test("Logs in a user", async function () {
     const user = { 
@@ -158,17 +158,163 @@ describe("POST /auth/login", function () {
   });
 });
 
+/** 
+ * Test GET /users route
+ */
+describe("GET /users", () => {
+  test("Gets all users", async () => {
+    const res = await request(app).get(`/users`);
+    const users = res.body.users;
+    expect(res.statusCode).toEqual(200);
+    expect(users).toBeInstanceOf(Array);
+    expect(users).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ username: "testuser-not-admin" }),
+        expect.objectContaining({ username: "testuser-admin" })
+      ])
+    );
+  });
+});
 
+/** 
+ * Test GET /users/:username route
+ */
+describe("GET /users/:username", () => {
+  test("Gets a single user's details", async () => {
+    const res = await request(app)
+      .get(`/users/testuser-not-admin`)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(200);
+    expect(user).toEqual(
+      expect.objectContaining({ username: "testuser-not-admin" })
+    );
+  });
 
+  test("404 if user cannot be found", async function() {
+    const res = await request(app)
+      .get(`/users/wronguser`)
+    ;
+    expect(res.statusCode).toBe(404);
+  });
+});
 
-// describe("GET /users", () => {
-//   test("Gets all users", async () => {
-//     const res = await request(app).get(`/users`);
-//     console.log("RES.BODY: ", res.body);
-//     const users = res.body.users;
-//     console.log("USERS: ", users);
-//     expect(res.statusCode).toEqual(200);
-//     expect(users).toBeInstanceOf(Array);
-//   });
-// https://www.codegrepper.com/code-examples/javascript/jest+test+array+of+objects
-// })
+/** 
+ * Test PATCH /users/:username route
+ */
+describe("PATCH /users/:username", () => {
+  test("Updates a user's email address", async () => {
+    const data = { 
+      user: 
+        { email: "newEmail@test.com" },
+      _token: TEST_DATA.notAdminToken
+      }
+    ;
+    const res = await request(app)
+      .patch(`/users/${TEST_DATA.notAdminUsername}`)
+      .send(data)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(200);
+    expect(user).toHaveProperty("username");
+    expect(user.email).toBe("newEmail@test.com");
+  });
+
+  test("Prohibits update of user's username", async () => {
+    const data = { 
+      user: 
+        { username: "newUsername" },
+      _token: TEST_DATA.notAdminToken
+    };
+    const res = await request(app)
+      .patch(`/users/${TEST_DATA.notAdminUsername}`)
+      .send(data)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(400);
+  });
+
+  test("Prohibits update of another user's email address", async () => {
+    const data = { 
+      user: 
+        { email: "newemail@email.com" },
+      _token: TEST_DATA.adminToken
+    };
+    const res = await request(app)
+      .patch(`/users/${TEST_DATA.notAdminUsername}`)
+      .send(data)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(401);
+  });
+
+  test("Prohibits a user from adding a new user property", async () => {
+    const data = { 
+      user: 
+        { cat: "Millie" },
+      _token: TEST_DATA.notAdminToken
+    };
+    const res = await request(app)
+      .patch(`/users/${TEST_DATA.notAdminUsername}`)
+      .send(data)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(400);
+  });
+
+  test("Responds with 404 error if the username in URL is not found", async () => {
+    const data = { 
+      user: 
+        { email: "newemail@email.com" },
+      _token: TEST_DATA.notAdminToken
+    };
+    // delete user first
+    await request(app)
+      .delete(`/users/${TEST_DATA.notAdminUsername}`)
+      .send({ _token: `${TEST_DATA.notAdminToken}` });
+    const res = await request(app)
+      .patch(`/users/${TEST_DATA.notAdminUsername}`)
+      .send(data)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(404);
+  });
+});
+
+/** 
+ * Test DELETE /users/:username route
+ */
+describe("DELETE /users/:username", () => {
+  test("Deletes a user", async () => {
+    const res = await request(app)
+      .delete(`/users/${TEST_DATA.notAdminUsername}`)
+      .send({ _token: `${TEST_DATA.notAdminToken}` })
+    ;
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: "User deleted" });
+  });
+  test("Prevents a user from deleting another user", async () => {
+    const res = await request(app)
+      .delete(`/users/${TEST_DATA.adminUsername}`)
+      .send({ _token: `${TEST_DATA.notAdminToken}` })
+    ;
+    expect(res.statusCode).toBe(401);
+  });
+  test("Responds with a 404 error if the user cannot be found", async () => {
+    const data = { 
+      user: 
+        { email: "newemail@email.com" },
+      _token: TEST_DATA.notAdminToken
+    };
+    // delete user first
+    await request(app)
+      .delete(`/users/${TEST_DATA.notAdminUsername}`)
+      .send({ _token: `${TEST_DATA.notAdminToken}` });
+    const res = await request(app)
+      .delete(`/users/${TEST_DATA.notAdminUsername}`)
+      .send(data)
+    ;
+    const user = res.body.user;
+    expect(res.statusCode).toEqual(404);
+  });
+});
