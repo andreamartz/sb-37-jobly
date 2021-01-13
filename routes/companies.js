97e5cdf7152e4f302eb90599f58291d180868c06
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-01-12 21:01:59 -0600

    Add descriptions of each route, including the structure of inputs and outputs

--- routes/companies.js
+++ routes/companies.js
@@ -9,6 +9,30 @@ const companySchemaUpdate = require("../schemas/companySchemaUpdate");
 const validateData = require("../helpers/validateData");
 const { authenticateJWT, authRequired, adminRequired } = require("../middleware/auth");
 
+/** 
+ * GET /: gets all companies
+ * 
+ * Auth: requires login
+ * 
+ * Returns object with companies key containing array of company objects
+ * 
+ * { _token: _token }
+ * =>
+ * { 
+ *   "companies": [
+ *     {
+ *        handle: handle,
+ *        name: name
+ *     },
+ *     {
+ *        handle: handle,
+ *        name: name
+ *     },
+ *     { ... },
+ *     { ... }
+ *   ]
+ * }
+ */
 router.get("/", authenticateJWT, authRequired, async function (req, res, next) {
   try {
     let {search, min_employees, max_employees} = req.query;
@@ -35,6 +59,45 @@ router.get("/", authenticateJWT, authRequired, async function (req, res, next) {
   }
 });
 
+/** 
+ * GET /:id gets a company's details
+ * 
+ * Auth: requires login
+ * 
+ * Returns object with "company" key that contains a company object with property jobs (an object containing an array of job objects)
+ * 
+ *  * { _token: _token }
+ * =>
+ * { 
+ *   company: {
+ *       handle: handle,
+ *       name: name,
+ *       num_employees: num_employees,
+ *       description: description,
+ *       logo_url: logo_url,
+ *     jobs: [
+ *       {
+ *         id: id,
+ *         title: title,
+ *         salary: salary,
+ *         equity: equity,
+ *         company_handle: company_handle,
+ *         date_posted: date_posted,}]
+ *       },
+ *       {
+ *         id: id,
+ *         title: title,
+ *         salary: salary,
+ *         equity: equity,
+ *         company_handle: company_handle,
+ *         date_posted: date_posted,}]
+ *       },
+ *       { ... },
+ *       { ... }
+ *     ]
+ *   }
+ * }
+ */
 router.get("/:handle", authenticateJWT, authRequired, async function (req, res, next) {
   try {
     const handle = req.params.handle.toUpperCase();
@@ -45,6 +108,33 @@ router.get("/:handle", authenticateJWT, authRequired, async function (req, res, 
   }
 });
 
+/** 
+ * POST / creates a new job
+ * 
+ * Auth: admin rights are required
+ * 
+ * { 
+ *   company: { 
+ *     handle: handle,
+ *     name: name,
+ *     num_employees: num_employees,
+ *     description: description,
+ *     logo_url: logo_url,
+ *   },
+ *   _token: token 
+ * } 
+ * => 
+ * { 
+ *   company: { 
+ *     handle: handle,
+ *     name: name,
+ *     num_employees: num_employees,
+ *     description: description,
+ *     logo_url: logo_url,
+ *   }
+ * } 
+ * 
+ */
 router.post("/", authenticateJWT, adminRequired, async function(req, res, next) {
   try {
     // validate data
@@ -65,12 +155,39 @@ router.post("/", authenticateJWT, adminRequired, async function(req, res, next) 
   }
 });
 
+/** 
+ * PATCH /:id updates a company's details
+ * 
+ * Auth needed: must be an admin
+ * 
+ * Fields that can be updated: name, num_employees, description, logo_url
+ * 
+ * {
+ *    company: {
+ *      name: name,
+ *      num_employees: num_employees,
+ *      description: description,
+ *      logo_url: logo_url,
+ *    },
+ *    _token: _token
+ *   }
+ * => 
+ * {
+ *    company: {
+ *      handle: handle,
+ *      name: name,
+ *      num_employees: num_employees,
+ *      description: description,
+ *      logo_url: logo_url
+ *    }
+ * } 
+ */
 router.patch("/:handle", authenticateJWT, adminRequired, async function (req, res, next) {
   try {
     const handle = req.params.handle.toUpperCase();
     const companyData = req.body.company;
 
-    // throw error if handle is not found for any company
+    // throw error if handle is not found for a company
     const companyCheck = await Company.findOne(handle);
 
     // validate the data on the request body
@@ -91,6 +208,21 @@ router.patch("/:handle", authenticateJWT, adminRequired, async function (req, re
   }
 });
 
+/** 
+ * DELETE /:id deletes a company
+ * 
+ * Auth needed: must be an admin
+ * 
+ * Input: _token
+ * 
+ * {
+ *    _token: _token
+ *  }
+ * => 
+ * {
+ *   message: "Company deleted"
+ * } 
+ */
 router.delete("/:handle", authenticateJWT, adminRequired, async function (req, res, next) {
   try {
     const handle = req.params.handle.toUpperCase();
